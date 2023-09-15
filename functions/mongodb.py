784aedf12f05674e483cd12b2a88f2d64f41eee6
Rémi <remi.dandois@gmail.com>
2023-09-15 19:59:38 -0400

    add 'experiences' routes

--- functions/mongodb.py
+++ functions/mongodb.py
@@ -7,36 +7,36 @@ load_dotenv()
 
 mongodb_srv = os.environ.get("MONGODB_SRV")
 
-def get_mongo_collection():
+def get_mongo_collection(coll):
     client = MongoClient(mongodb_srv)
     db = client["data"]
-    collection = db["faq"]
+    collection = db[coll]
     return collection
 
-def find_document(id):
-    collection = get_mongo_collection()
+def find_document(coll, id):
+    collection = get_mongo_collection(coll)
     document = collection.find_one({"_id": ObjectId(id)})
     return document
 
-def all_documents():
-    collection = get_mongo_collection()
+def all_documents(coll):
+    collection = get_mongo_collection(coll)
     documents = collection.find()
     docs =[]
     for doc in documents:
         docs.append(doc)
     return docs
 
-def insert_document(document):
-    collection = get_mongo_collection()
+def insert_document(coll, document):
+    collection = get_mongo_collection(coll)
     inserted_document = collection.insert_one(document)
     return inserted_document
 
-def delete_decument(id):
-    collection = get_mongo_collection()
+def delete_decument(coll, id):
+    collection = get_mongo_collection(coll)
     deleted_document = collection.delete_one({"_id": ObjectId(id)})
     return deleted_document
 
-def update_document(id, update):
-    collection = get_mongo_collection()
+def update_document(coll, id, update):
+    collection = get_mongo_collection(coll)
     updated_document = collection.update_one({"_id": ObjectId(id)}, { "$set": update})
     return updated_document
--- routes/experiences.py
+++ routes/experiences.py
@@ -3,27 +3,27 @@ from flask import request
 import functions.mongodb as mongodb
 import functions.utils as utils
 
-api = Namespace('faq', description='Faq related operations')
+api = Namespace('experiences', description='Experiences related operations')
 
 @api.route("/")
-class faqs(Resource):
+class experiences(Resource):
     def get(self):
-        documents = mongodb.all_documents("faq")
+        documents = mongodb.all_documents("experiences")
         return utils.send_json(documents)
     
     def post(self):
-        inserted_document = mongodb.insert_document("faq", request.json)
+        inserted_document = mongodb.insert_document("experiences", request.json)
         report_id = str(inserted_document.inserted_id)
-        return utils.send_json({"msg": "faq entry succefully saved in db.", "inserted_id": report_id})
+        return utils.send_json({"msg": "experience entry succefully saved in db.", "inserted_id": report_id})
     
 @api.route("/id/<id>")
-class faq(Resource):
+class experience(Resource):
     def get(self, id):
-        document = mongodb.find_document("faq", id)
+        document = mongodb.find_document("experiences", id)
         return utils.send_json(document) if ( (isinstance(document, dict)) and (len(document) != 0) ) else utils.send_json({})
     
     def delete(self, id):
-        deleted_doc = mongodb.delete_decument("faq", id)
+        deleted_doc = mongodb.delete_decument("experiences", id)
         msg = "document succesfully deleted" if deleted_doc.deleted_count == 1 else "no document found for deletion"
         return utils.send_json({"msg": msg})
     
@@ -32,7 +32,7 @@ class faq(Resource):
         update = utils.check_body(body)
         if (len(update) == 0):
             return utils.send_json({"status": "fail", "msg": "update query empty, please provide non empty json object"})
-        updated_doc = mongodb.update_document("faq", id, update)
+        updated_doc = mongodb.update_document("experiences", id, update)
         msg = ""
         if updated_doc.matched_count == 0:
             msg = "no document found for update"

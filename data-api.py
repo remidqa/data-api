dd0ab503b21c021147a3e7860da93c3bb28ff5e8
Rémi <remi.dandois@gmail.com>
2023-09-16 22:44:20 -0400

    refacto route files

--- data-api.py
+++ data-api.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restx import Resource, Api
-from routes.faq import api as ns_faq
+from routes.crud import api as ns_cruds
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_namespace(ns_faq, path='/faq')
+api.add_namespace(ns_cruds, path='/')
 
 
 
--- routes/crud.py
+++ routes/crud.py
@@ -3,36 +3,37 @@ from flask import request
 import functions.mongodb as mongodb
 import functions.utils as utils
 
-api = Namespace('faq', description='Faq related operations')
 
-@api.route("/")
-class faqs(Resource):
-    def get(self):
-        documents = mongodb.all_documents("faq")
+api = Namespace('mongodb crud', description='Mongo related operations')
+
+@api.route("/<coll_name>")
+class curds(Resource):
+    def get(self, coll_name):
+        documents = mongodb.all_documents(coll_name)
         return utils.send_json(documents)
     
-    def post(self):
-        inserted_document = mongodb.insert_document("faq", request.json)
+    def post(self, coll_name):
+        inserted_document = mongodb.insert_document(coll_name, request.json)
         report_id = str(inserted_document.inserted_id)
-        return utils.send_json({"msg": "faq entry succefully saved in db.", "inserted_id": report_id})
+        return utils.send_json({"msg": f"{coll_name} entry succefully saved in db.", "inserted_id": report_id})
     
-@api.route("/id/<id>")
-class faq(Resource):
-    def get(self, id):
-        document = mongodb.find_document("faq", id)
+@api.route("<coll_name>/id/<id>")
+class curd(Resource):
+    def get(self, coll_name, id):
+        document = mongodb.find_document(coll_name, id)
         return utils.send_json(document) if ( (isinstance(document, dict)) and (len(document) != 0) ) else utils.send_json({})
     
-    def delete(self, id):
-        deleted_doc = mongodb.delete_decument("faq", id)
+    def delete(self, coll_name, id):
+        deleted_doc = mongodb.delete_decument(coll_name, id)
         msg = "document succesfully deleted" if deleted_doc.deleted_count == 1 else "no document found for deletion"
         return utils.send_json({"msg": msg})
     
-    def put(self, id):
+    def put(self, coll_name, id):
         body = request.get_json(force=True, silent=True)
         update = utils.check_body(body)
         if (len(update) == 0):
             return utils.send_json({"status": "fail", "msg": "update query empty, please provide non empty json object"})
-        updated_doc = mongodb.update_document("faq", id, update)
+        updated_doc = mongodb.update_document(coll_name, id, update)
         msg = ""
         if updated_doc.matched_count == 0:
             msg = "no document found for update"
